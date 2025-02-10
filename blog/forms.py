5635e204ea9c8658c2174f92c062582f883df11e
ChristianJehoshaphatS <christian.suryo@phiraka.com>
2025-02-10 14:03:50 +0700

    Add validation and comment model

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,31 @@ class PostForm(forms.ModelForm):
     class Meta:
         model= Post
         fields = ('title','text')
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get('title') != cleaned_data.get('title').upper():
+            raise forms.ValidationError('Title Must be uppercase')
+        
+        return self.cleaned_data
+    
+    def clean_text(self):
+        cleaned_data = super().clean()
+        text = cleaned_data.get('text')
+        if len(text) >= 100:
+            raise forms.ValidationError('Text must be less than 100 characters')
+        return text
+    
+    
+
+# PR: Post form pakai forms.Form biasa, nggak pakai ModelForm
+from django import forms
+
+class CommentForm(forms.Form):
+    author = forms.CharField(
+        max_length=200,  # Increased max length for author name
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}) #nice input field with placeholder
+    )
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Comment', 'rows': 3}) #nice textarea input field with placeholder
+    )
--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True) # null buat db, blank buat form, ngaruh buat admin
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes')
     
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +18,16 @@ class Post(models.Model):
         return self.title
     
     
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
